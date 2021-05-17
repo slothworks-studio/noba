c6a55822831a8250cdbefea7f78f2b86b81aa97d
Tom <tom@tm3.solutions>
2021-05-17 14:33:32 -0400

    .

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,10 @@ const config = {
     adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    files: {
+      serviceWorker: 'serviceWorker.js',
+      template: 'src/app.html',
+    },
   },
 };
 
